901614ed5f8abc408aae485d94312028e7bd0851
MengseuThoeng <mengseu2004@gmail.com>
2024-12-20 11:09:53 +0700

    Deploy

--- src/test/java/co/istad/eurekaservice/EurekaServiceApplicationTests.java
+++ src/test/java/co/istad/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package co.istad.eurekaservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package co.istad.eurekaservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+////
+////@SpringBootTest
+////class EurekaServiceApplicationTests {
+////
+////    @Test
+////    void contextLoads() {
+////    }
+////
+////}
